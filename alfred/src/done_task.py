48ba38f766050ba9a9c0c875f63c51952a0a2bdb
Martyn <zogacc@gmail.com>
2021-05-10 20:46:11 +0200

    add new command for workflow

--- alfred/src/done_task.py
+++ alfred/src/done_task.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+from datetime import datetime
 
 from notion_api import notion_api
 
@@ -11,8 +12,12 @@ try:
     args = parser.parse_args(sys.argv[1].split())
 
     query = ' '.join(args.query)
+    date_time = datetime.now().strftime("%d/%m/%Y, %H:%M")
+    query_with_time = query + " @" + date_time
 
-    print(notion_api.append_task_to_notes(query, True).title, query)
+    response = notion_api.append_task_to_notes(query_with_time, True).title
+    # print(response, query)
+    sys.stdout.write(query)
 except Exception as e:
-    # Print out nothing on STDOUT (missing value means means operation was unsuccessful)
-    sys.stderr.write(e)
+    # Print out nothing on STDOUT (missing value means operation was unsuccessful)
+    sys.stdout.write()
